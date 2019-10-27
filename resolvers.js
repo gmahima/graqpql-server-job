9a2b34ba0240e58983f200d986770e58254e64bd
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-10-27 12:31:44 +0530

    add mutation resolver

--- resolvers.js
+++ resolvers.js
@@ -7,10 +7,15 @@ const Query = {
   company: (root, { id }) => db.companies.get(id),
   jobs: () => db.jobs.list()
 };
+const Mutation = {
+  createJob: (root, { companyId, title, description }) => {
+    return db.jobs.create({ companyId, title, description });
+  }
+};
 const Job = {
   company: job => db.companies.get(job.companyId)
 };
 const Company = {
   jobs: company => db.jobs.list().filter(job => job.companyId === company.id)
 };
-module.exports = { Query, Job, Company };
+module.exports = { Query, Mutation, Job, Company };
